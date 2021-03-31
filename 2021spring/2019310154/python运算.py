946784c8cf92aa1d526e7a23a217ac908635d7fc
YunHong-Wang <79582326+YunHong-Wang@users.noreply.github.com>
2021-03-31 19:00:34 +0800

    Create python运算.py

---
+++ 2021spring/2019310154/python运算.py
@@ -0,0 +1,57 @@
+'加（减）法'
+>>> a=1
+>>> b=1
+>>> print(a+b)
+2
+
+'乘法'
+>>> a=1
+>>> b=3
+>>> print(a*b)
+3
+
+'除法'
+>>> a=4
+>>> b=2
+>>> print(a/b)
+2.0
+
+'幂的运算'
+>>> a=2
+>>> b=3
+>>> print(a**b)
+8
+
+'平方根'
+>>> import math
+>>> math.sqrt(a)
+>>> a=3
+>>> math.sqrt(a)
+1.7320508075688772
+
+>>> '取绝对值'
+>>> import math
+>>> a=-3
+>>> print(abs(-3))
+3
+
+>>> '余数'
+>>> a=2
+>>> b=9
+>>> print(b//a)
+4
+
+>>> '对数'
+>>> import math
+>>> math.log(a,10)
+0.30102999566398114
+>>> a=7
+>>> print(math.log(a,10))
+0.8450980400142567
+
+>>> '三角函数'
+>>> import math
+>>> math.asin
+<built-in function asin>
+>>> a=7
+>>> print(math(sin(a))
